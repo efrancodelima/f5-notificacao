b339cf4ad85638d5665ae5a1114c6af2cad2a8b1
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-03-30 11:55:28 -0300

    Melhoria do teste de HomeController

--- src/test/java/br/com/fiap/soat/controller/HomeControllerTest.java
+++ src/test/java/br/com/fiap/soat/controller/HomeControllerTest.java
@@ -3,11 +3,8 @@ package br.com.fiap.soat.controller;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -16,24 +13,14 @@ import io.restassured.RestAssured;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
 
-  AutoCloseable closeable;
   String endpoint = "/";
 
   @LocalServerPort
   private int port;
 
-  @InjectMocks
-  private HomeController homeController;
-
   @BeforeEach
   void setup() {
-    closeable = MockitoAnnotations.openMocks(this);
     RestAssured.port = port;
-  }
-
-  @AfterEach
-  void tearDown() throws Exception {
-    closeable.close();
   }
 
   @Test
